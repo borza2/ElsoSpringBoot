c24e4e767f1b490e9926865a68f741eeaae99e63
dav <dvidborza@gmail.com>
2018-12-20 13:53:28 +0100

    23

--- src/main/java/com/elsoproject/controller/HomeController.java
+++ src/main/java/com/elsoproject/controller/HomeController.java
@@ -24,20 +24,20 @@ public class HomeController {
 		
 		ArrayList<Story> stories = new ArrayList<Story>();
 		
-		Story story1 = new Story();
-		story1.setTitle("Első Sztori!");
-		story1.setPosted(new Date());
-		story1.setAuthor("Dávid");
-		story1.setContent("<p>Ez az első éles poszt tartalma!!</p>");
-		
-		Story story2 = new Story();
-		story2.setTitle("Második Sztori!");
-		story2.setPosted(new Date());
-		story2.setAuthor("Dávid");
-		story2.setContent("<p>Ez már nem olyan izgi.</p>");
-		
-		stories.add(story1);
-		stories.add(story2);
+//		Story story1 = new Story();
+//		story1.setTitle("Első Sztori!");
+//		story1.setPosted(new Date());
+//		story1.setAuthor("Dávid");
+//		story1.setContent("<p>Ez az első éles poszt tartalma!!</p>");
+//		
+//		Story story2 = new Story();
+//		story2.setTitle("Második Sztori!");
+//		story2.setPosted(new Date());
+//		story2.setAuthor("Dávid");
+//		story2.setContent("<p>Ez már nem olyan izgi.</p>");
+//		
+//		stories.add(story1);
+//		stories.add(story2);
 		
 		return  stories;
 		
